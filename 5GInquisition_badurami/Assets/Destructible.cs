0b57fcb63e39d6edf3f57c98a69817fa0363b8cb
mibadura <mibadura@gmail.com>
2020-05-13 23:55:59 +0200

    Pieces can be punched now. Only box works somehow

--- 5GInquisition_badurami/Assets/Destructible.cs
+++ 5GInquisition_badurami/Assets/Destructible.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class Destructible : MonoBehaviour
 {
     public GameObject destroyedVersion;
-    
 
     void OnMouseDown()
     {
-        
-        // ReSharper disable once Unity.InefficientPropertyAccess
-        Destroy(gameObject);
-        var transformInfo = gameObject.transform;
-        Instantiate(destroyedVersion, transformInfo.position, transformInfo.rotation);
-        Debug.Log(transformInfo.position);
-        Debug.Log("Destructible Script Used");
-        //var rb = destroyedVersion.GetComponent<Rigidbody>();
-        //rb.AddForce(Camera.main.transform.forward*100, ForceMode.Impulse);
+        if (destroyedVersion)
+        {
+            Debug.Log("Destructible Script Used");
+            Destroy(gameObject);
+            var transformInfo = gameObject.transform;
+            Instantiate(destroyedVersion, transformInfo.position, transformInfo.rotation);
+
+        }
+        else
+        {
+            Debug.Log("Destructible Script Used On Pieces");
+            var rb = GetComponent<Rigidbody>();
+            rb.AddForce(Camera.main.transform.forward*100, ForceMode.Impulse);
+        }
     }
     
   
