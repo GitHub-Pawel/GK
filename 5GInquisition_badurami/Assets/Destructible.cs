6e7ca28a909c9d5d6bc9bef517520b5e12e327fc
mibadura <mibadura@gmail.com>
2020-05-13 22:55:10 +0200

    Simple tower destruction

---
+++ 5GInquisition_badurami/Assets/Destructible.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+    
+
+    void OnMouseDown()
+    {
+        
+        // ReSharper disable once Unity.InefficientPropertyAccess
+        Destroy(gameObject);
+        var transformInfo = gameObject.transform;
+        Instantiate(destroyedVersion, transformInfo.position, transformInfo.rotation);
+        Debug.Log(transformInfo.position);
+        Debug.Log("Destructible Script Used");
+        //var rb = destroyedVersion.GetComponent<Rigidbody>();
+        //rb.AddForce(Camera.main.transform.forward*100, ForceMode.Impulse);
+    }
+    
+  
+   
+}
---
+++ 5GInquisition_badurami/Assets/Pusher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    //public Rigidbody destroyedobject;
+
+    void OnMouseDown()
+    {
+        //Time.timeScale = 0.8f;
+        //var rb = GetComponent<Rigidbody>();
+        //destroyedobject.AddForce(Camera.main.transform.forward*100, ForceMode.Impulse);
+        Debug.Log("Pusher Script Used");
+    }
+}
