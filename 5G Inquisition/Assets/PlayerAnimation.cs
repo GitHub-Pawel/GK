2ef0a527459a3a5031e17a90252b8b3520510073
GitHub-Pawel <pawel.borzecin@gmail.com>
2020-05-15 01:54:18 +0200

    Attack options added

---
+++ 5G Inquisition/Assets/PlayerAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start() {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update() {
+        playAttackAnimation();
+        playGrenadeAnimation();
+    }
+
+    public void playAttackAnimation() {
+        if (Input.GetMouseButtonDown(0)) {
+            animator.SetTrigger("triggerAttack01");
+        }
+    }
+    
+    public void playGrenadeAnimation() {
+        if (Input.GetMouseButtonDown(1)) {
+            animator.SetTrigger("triggerAttack02");
+        }
+    }
+}
