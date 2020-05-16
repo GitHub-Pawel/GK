bd41846e4247faf733b55abd3402ac3d134f3fb5
mibadura <mibadura@gmail.com>
2020-05-16 20:18:22 +0200

    Changes in main project. Towers are now PREFABS. Destructible and punchable

---
+++ Grenade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float delay = 3f;
+    public GameObject explosionEffect;
+    private float countdown;
+    private bool hasExploded;
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            
+        }
+    }
+
+    private void Explode()
+    {
+        hasExploded = true;
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+    }
+}
