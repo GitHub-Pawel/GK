130e55f72195081cd4b74fdd0f4da47f4cde244b
GitHub-Pawel <pawel.borzecin@gmail.com>
2020-05-26 22:44:37 +0200

    Tower life bar follows the player position

---
+++ 5G Inquisition/Assets/Scripts/TowerCanvasRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerCanvasRotate : MonoBehaviour
+{
+    public Transform _target;
+
+    void Update()
+    {
+        lookAtTarget(_target);
+    }
+
+    public void lookAtTarget(Transform target)
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+    }
+}
