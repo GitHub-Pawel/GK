c18ae84ad742aa1e8f2c46571c5c4806c723a988
mibadura <mibadura@gmail.com>
2020-06-02 16:46:49 +0200

    Nice textures and Spinning Script

---
+++ 5G Inquisition/Assets/Scripts/Spinning.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class Spinning : MonoBehaviour
+{
+    public int SpinSpeed = 1;
+
+    private Transform transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotationVector = transform.rotation.eulerAngles;
+        rotationVector.y += SpinSpeed;
+        transform.rotation = Quaternion.Euler(rotationVector);
+        //transform.rotation = Quaternion.Euler(0,0, 1);
+    }
+}
